9faee9c08046392cc64839e02632d1ad1d4b70cb
PeachFruitDev <cirvric@gmail.com>
2024-10-26 12:55:40 +0400

    Added Tests and Reporter's Implementation
    - Implemented Reporter

--- src/main/java/test/interview/reporter/OrgStructReporter.java
+++ src/main/java/test/interview/reporter/OrgStructReporter.java
@@ -2,15 +2,90 @@ package test.interview.reporter;
 
 import test.interview.model.Employee;
 import test.interview.model.report.FullOrgStructReport;
+import test.interview.model.report.ManagerSalaryLessReport;
+import test.interview.model.report.ManagerSalaryMoreReport;
+import test.interview.model.report.ReportingLineReport;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class OrgStructReporter {
+
+    private final String salaryMinProp = "managers.salary.min.percent";
+    private final String salaryMaxProp = "managers.salary.max.percent";
+    private final String lineMaxProp = "managers.line.max";
 
     public FullOrgStructReport report(Employee ceo, Properties props) {
         if (ceo == null) {
             throw new ReporterException("CEO can't be null");
         }
-        return new FullOrgStructReport(null, null, null);
+
+        // As this app is used for internal analysis, I assume we don't need rigorously check props, only simple check
+        // is included
+        try {
+            var salaryMin = new BigDecimal(props.getProperty(salaryMinProp));
+            var salaryMax = new BigDecimal(props.getProperty(salaryMaxProp));
+            var lineMax = Integer.valueOf(props.getProperty(lineMaxProp));
+
+            return traverseTreeReporting(ceo, salaryMin, salaryMax, lineMax, -1);
+        } catch (NumberFormatException e) {
+            throw new ReporterException("Properties should be numeric");
+        }
+    }
+
+    private FullOrgStructReport traverseTreeReporting(Employee employee, BigDecimal salaryMin, BigDecimal salaryMax, Integer lineMax, Integer curLine) {
+        if (employee == null) {
+            throw new ReporterException("Employee can't be null");
+        }
+
+        var salaryLessReport = new ArrayList<ManagerSalaryLessReport>();
+        var salaryMoreReport = new ArrayList<ManagerSalaryMoreReport>();
+        var reportingLineReport = new ArrayList<ReportingLineReport>();
+
+        if (employee.getSubordinates() != null && !employee.getSubordinates().isEmpty()) {
+            var salaries = employee.getSubordinates().stream()
+                    .map(Employee::getSalary)
+                    .filter(Objects::nonNull)
+                    .toList();
+
+            if (employee.getSubordinates().size() > salaries.size()) {
+                throw new ReporterException("Salary can't be null; Employees' IDs: " + employee.getSubordinates().stream()
+                        .filter(subordinate -> subordinate.getSalary() == null)
+                        .map(Employee::getId)
+                        .map(Object::toString)
+                        .collect(Collectors.joining(",")));
+            }
+
+            var total = salaries.stream()
+                    .reduce(BigDecimal::add)
+                    .get();
+
+            var avg = total.divide(BigDecimal.valueOf(salaries.size()), 2, RoundingMode.HALF_EVEN);
+            var min = avg.multiply(salaryMin);
+            var max = avg.multiply(salaryMax);
+
+            if (employee.getSalary().compareTo(min) < 0) {
+                salaryLessReport.add(new ManagerSalaryLessReport(employee, employee.getSalary().subtract(min)));
+            } else if (employee.getSalary().compareTo(max) > 0) {
+                salaryMoreReport.add(new ManagerSalaryMoreReport(employee, employee.getSalary().subtract(max)));
+            }
+
+            employee.getSubordinates().forEach(subordinate -> {
+                var report = traverseTreeReporting(subordinate, salaryMin, salaryMax, lineMax, curLine + 1);
+                salaryLessReport.addAll(report.salaryLessReport());
+                salaryMoreReport.addAll(report.salaryMoreReport());
+                reportingLineReport.addAll(report.reportingLineReport());
+            });
+        }
+
+        if (curLine > lineMax) {
+            reportingLineReport.add(new ReportingLineReport(employee, curLine - lineMax));
+        }
+
+        return new FullOrgStructReport(salaryLessReport, salaryMoreReport, reportingLineReport);
     }
 }
--- src/test/java/OrgStructReporterTest.java
+++ src/test/java/OrgStructReporterTest.java
@@ -8,16 +8,15 @@ import java.util.List;
 import java.util.Properties;
 
 import static java.util.Collections.emptyList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrgStructReporterTest {
 
     private final OrgStructReporter reporter = new OrgStructReporter();
     private final Properties props = new Properties() {{
-        put("managers.salary.min.percent", 1.2);
-        put("managers.salary.max.percent", 1.5);
-        put("managers.line.max", 1);
+        setProperty("managers.salary.min.percent", "1.2");
+        setProperty("managers.salary.max.percent", "1.5");
+        setProperty("managers.line.max", "1");
     }};
 
     @Test
@@ -89,8 +88,7 @@ public class OrgStructReporterTest {
                 1,
                 emptyList());
 
-        var ceo =
-                new Employee(1,
+        var ceo = new Employee(1,
                         "Joe",
                         "Doe",
                         new BigDecimal("1.15"),
@@ -104,7 +102,7 @@ public class OrgStructReporterTest {
         assertEquals(report.reportingLineReport().size(), 0);
 
         assertEquals(report.salaryLessReport().get(0).employee(), ceo);
-        assertEquals(report.salaryLessReport().get(0).difference(), new BigDecimal("-5.45"));
+        assertTrue(report.salaryLessReport().get(0).difference().compareTo(new BigDecimal("-5.45")) == 0);
     }
 
     @Test
@@ -123,8 +121,7 @@ public class OrgStructReporterTest {
                 1,
                 emptyList());
 
-        var ceo =
-                new Employee(1,
+        var ceo = new Employee(1,
                         "Joe",
                         "Doe",
                         new BigDecimal("8.8"),
@@ -138,7 +135,7 @@ public class OrgStructReporterTest {
         assertEquals(report.reportingLineReport().size(), 0);
 
         assertEquals(report.salaryMoreReport().get(0).employee(), ceo);
-        assertEquals(report.salaryMoreReport().get(0).difference(), new BigDecimal("0.55"));
+        assertTrue(report.salaryMoreReport().get(0).difference().compareTo(new BigDecimal("0.55")) == 0);
     }
 
     @Test
@@ -146,15 +143,13 @@ public class OrgStructReporterTest {
 
         var subordinate3 = new Employee(4,
                 "Joe3",
-                "Doe3",
-                BigDecimal.TEN,
+                "Doe3", new BigDecimal("6"),
                 3,
                 emptyList());
 
         var subordinate2 = new Employee(3,
                 "Joe3",
-                "Doe3",
-                BigDecimal.TEN,
+                "Doe3", new BigDecimal("8"),
                 2,
                 List.of(subordinate3));
 
@@ -165,8 +160,7 @@ public class OrgStructReporterTest {
                 1,
                 List.of(subordinate2));
 
-        var ceo =
-                new Employee(1,
+        var ceo = new Employee(1,
                         "Joe",
                         "Doe",
                         new BigDecimal("13"),
@@ -189,5 +183,22 @@ public class OrgStructReporterTest {
 
         var e = assertThrows(ReporterException.class, () -> reporter.report(ceo, props));
         assertEquals(e.getMessage(), "CEO can't be null");
+    }
+
+    @Test
+    public void reporterTest_nullSalary_success() {
+        var subordinate1 = new Employee(2, "Joe2", "Doe2", null, 1, emptyList());
+
+        var subordinate2 = new Employee(3, "Joe3", "Doe3", null, 1, emptyList());
+
+        var ceo = new Employee(1,
+                "Joe",
+                "Doe",
+                new BigDecimal("7.15"),
+                null,
+                List.of(subordinate1, subordinate2));
+
+        var e = assertThrows(ReporterException.class, () -> reporter.report(ceo, props));
+        assertEquals(e.getMessage(), "Salary can't be null; Employees' IDs: 2,3");
     }
 }
